67b2898ca1e82b265a02e90fb04931e3e713c3f5
Kaze <godisevil2005@gmail.com>
2026-08-27 11:24:19 +0000

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from nicegui import ui
 ui.query('body').classes('m-0')
 
+from nicegui import app
+app.add_static_files('/assets', 'assets')
+
 with ui.element('div').classes(
     "w-full h-screen flex items-center justify-center bg-slate-950"
 ):
-    ui.image
-    with ui.card().classes(
-        "w-100 items-center p-10"
-    ):
-        ui.label("Arashi-OS").classes(
+        with ui.card().classes(
+    "w-100 items-left"
+        ):
+            ui.image('/assets/storm_background.jpg'
+            ).classes("w-full h-40 rounded-lg mb-4")
+            ui.label("Arashi-OS").classes(
             "text-4xl font-bold"
-        )
-
-        ui.label("intialising Arashi-OS....").classes(
-            "text-grey-400"
-        )
-
-        ui.spinner(size='lg')
-
+            )
+            with ui.row().classes("w-full justify-between"
+            ):
+                ui.label("intialising Arashi-OS....").classes(
+                "text-gray-400"
+                )
+                ui.spinner(size='lg')
 
 ui.run()
